8b464ee1338a8538b9206be3adf8d44f3c19b72d
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-03-17 19:45:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function calculateMinCost() {
   //your code here
-  
+
+	 let str = document.querySelector("#rope-lengths").value;
+    let arr = str.split(", ");
+
+    for(let i in arr){
+        arr[i] =  Number(arr[i]);
+    }
+     let mincost=0;
+    while(arr.length!=1){
+        arr.sort( function (a,b){
+            return a-b;
+        });
+        let cost= arr[0] + arr[1];
+        mincost+=cost;
+        arr.splice(0,2);
+        arr.push(cost);
+    }
+
+
+    document.querySelector("#result").innerHTML=mincost;
   
   
 }  
